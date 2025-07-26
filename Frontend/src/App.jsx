725b1b44072ab513e22b91f4f7bff28b5417d384
HusnainDot <husnainmaroof95@gmail.com>
2025-07-26 09:42:38 +0500

    rigestration form setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./globals.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
