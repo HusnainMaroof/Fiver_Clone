aa5127d1d56b09c39960d3ca7cf094d202ce40be
HusnainDot <husnainmaroof95@gmail.com>
2025-07-31 19:15:32 +0500

    changes

--- Frontend/src/Components/Otp.jsx
+++ Frontend/src/Components/Otp.jsx
@@ -8,6 +8,9 @@ const Otp = () => {
   const inputRefs = useRef([]);
   const [validOTP, setValidOTP] = useState(false);
   const [succes, setSucces] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resend, setResend] = useState(false);
+  const [resendTimer, setResendTimer] = useState(30);
   useEffect(() => {
     setRegPopUpPic({
       intalsPic: false,
@@ -41,18 +44,33 @@ const Otp = () => {
 
     if (enteredOtp.length >= 6) {
       setValidOTP(true);
-
     } else {
       setValidOTP(false);
     }
   }, [otp]);
 
+  const handleResend = () => {
+    setOtp(["", "", "", "", "", ""]);
+    setResend(true);
+    setResendTimer(30);
+
+    const timerInterval = setInterval(() => {
+      setResendTimer((prev) => {
+        if (prev <= 1) {
+          clearInterval(timerInterval);
+          setResend(false);
+          return 30;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  };
 
   const handelSubmit = () => {};
 
   return (
     <>
-      <div className="bg-white w-full flex flex-col justify-between h-full rounded-l-lg p-4 ">
+      <div className="bg-white w-auto flex flex-col justify-between h-full rounded-l-lg p-4 ">
         <div className="">
           <div>
             <h3 className="text-xl font-semibold py-3">Confirm your email</h3>
@@ -65,27 +83,46 @@ const Otp = () => {
               email@gmail.com.
             </p>
           </div>
-          <div className="flex items-center justify-center my-10">
-            <div className="flex space-x-2 mb-4 ">
-              {otp.map((digit, index) => (
-                <input
-                  key={index}
-                  type="text"
-                  inputMode="numeric"
-                  maxLength="1"
-                  value={digit}
-                  onChange={(e) => handleChange(e, index)}
-                  onKeyDown={(e) => handleKeyDown(e, index)}
-                  ref={(el) => (inputRefs.current[index] = el)}
-                  className="w-12 h-12 text-2xl text-center border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-800"
-                />
-              ))}
-            </div>
+          <div className="flex items-center justify-center gap-x-1 mt-10">
+            {otp.map((digit, index) => (
+              <input
+                key={index}
+                type="text"
+                inputMode="numeric"
+                maxLength="1"
+                value={digit}
+                onChange={(e) => handleChange(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+                ref={(el) => (inputRefs.current[index] = el)}
+                className={`w-10 md:w-12 h-10 md:h-12 text-2xl text-center border  rounded focus:outline-none focus:ring-2 focus:ring-gray-800 ${
+                  succes ? "border-red-500" : "border-gray-300"
+                } `}
+              />
+            ))}
           </div>
+          {succes && (
+            <p className="text-red-500 py-2 font-semibold my-1.5">
+              You've entered the wrong code. Try again.
+            </p>
+          )}
+
+          <button
+            onClick={handleResend}
+            type="button"
+            disabled={resend}
+            className={`underline font-semibold my-1.5 text-md ${
+              resend
+                ? "text-gray-400 cursor-not-allowed "
+                : "text-gray-800 cursor-pointer"
+            }`}
+          >
+            {resend && <span className="px-1 ">{resendTimer}s</span>}
+            Resend Code
+          </button>
         </div>
 
         <div className="w-full  flex items-baseline justify-between">
-          <p className="underline font-bold text-lg text-gray-700 cursor-pointer">
+          <p className="underline font-bold   md:text-lg text-gray-700 cursor-pointer">
             Remind me later
           </p>
 
@@ -99,7 +136,7 @@ const Otp = () => {
                 : "border-gray-800 bg-gray-800 cursor-pointer text-white "
             }`}
           >
-            {succes ? <PuffLoader size={20} color="#ffffff" /> : " Submit"}
+            {loading ? <PuffLoader size={20} color="#ffffff" /> : " Submit"}
           </button>
         </div>
       </div>
